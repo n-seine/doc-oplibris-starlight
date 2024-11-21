163e4d7835fbb40935c8c691ea73a94f4a68d4ba
Nicolas Seine <contact@nicolasseine.com>
2024-11-21 12:53:00 +0100

    corrige segment  de base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://n-seine.github.io",
-  base: "docs-oplibris",
+  base: "/docs-oplibris",
   integrations: [
     starlight({
       title: "My Docs",
